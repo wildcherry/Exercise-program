ab8f5c6b188c98c377d076e2f2640b25fc4c5d4b
wildcherry <wild_cherry@163.com>
2016-03-08 21:27:53 +0800

    work

---
+++ e-commerce_sites/js/ajax.js
@@ -0,0 +1,6 @@
+$(".nav-left ul li:first-child").click(function(){
+	$.ajax({
+		type:"get",  //ajax请求的方式
+		url:"data/main.json",                                                                                 
+	
+})
